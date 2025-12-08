37342da439a19aa9c1680edda1fb8d22f38894f0
Maxime <maxime.lpz@outlook.fr>
2025-12-08 16:30:53 +0100

    login

--- screens/Login.tsx
+++ screens/Login.tsx
@@ -1,27 +1,76 @@
-import React from 'react';
+import { useState } from 'react';
 import { View, Text, Button, TextInput } from 'react-native';
+import Constants from "expo-constants";
 
 type Props = {
   navigation: any;
   setIsLoggedIn: (value: boolean) => void;
 };
 
+const API_URL = Constants.expoConfig?.extra?.API_URL;
+
+if (!API_URL) {
+  console.warn("⚠️ API_URL is missing in app.config.js or .env");
+}
+
 export default function Login({ navigation, setIsLoggedIn }: Props) {
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const login = async (email: string, password: string) => {
+    if (! email || !password) {
+      setErrorMessage('Veuillez remplir tous les champs.');
+      return;
+    }
+
+    try {
+      const response = await fetch(API_URL + 'users/login', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      const data = await response.json();
+
+      if (response.ok) {
+        setIsLoggedIn(true);
+      } else {
+        setErrorMessage(data.error || data.message || 'Identifiants incorrects.');
+      }
+
+    } catch (error: any) {
+      console.error('Erreur login :', error.message);
+    }
+  };
+
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center', padding: 20 }}>
       <Text style={{ fontSize: 24, marginBottom: 20 }}>Connexion</Text>
       
       <TextInput
         placeholder="Email"
+        value={email}
+        onChangeText={value => setEmail(value)}
+        keyboardType="email-address"
+        autoCapitalize="none"
+        autoCorrect={false}
         style={{ width: '100%', borderWidth: 1, borderColor: '#ccc', padding: 10, marginBottom: 10 }}
       />
       <TextInput
         placeholder="Mot de passe"
+        value={password}
+        onChangeText={value => setPassword(value)}
         secureTextEntry
         style={{ width: '100%', borderWidth: 1, borderColor: '#ccc', padding: 10, marginBottom: 20 }}
       />
 
-      <Button title="Se connecter" onPress={() => setIsLoggedIn(true)} />
+      {errorMessage && <Text style={{ color: 'red' }}>{errorMessage}</Text>}
+      <View style={{ height: 10 }} />
+
+      <Button title="Se connecter" onPress={() => login(email, password)} />
       <View style={{ height: 10 }} />
       <Text onPress={() => navigation.navigate('Signup')} >Pas encore de compte ? S'inscrire</Text>
     </View>
