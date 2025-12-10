84219c0b8132675528ac542497a47767781f1f06
Théo <theo.rgr2404@gmail.com>
2025-12-10 14:02:26 +0100

    profil

--- screens/Home.tsx
+++ screens/Home.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import { View, Text, ScrollView } from 'react-native';
 import { useAuthStore } from '../store/useAuthStore';
 import DonutProgress from '../components/DonutProgress';
@@ -8,63 +9,65 @@ export default function Home() {
   const user = useAuthStore(state => state.user);
 
   return (
-    <ScrollView
-      className="flex-1 bg-white"
-      showsVerticalScrollIndicator={false}
-      contentContainerStyle={{ paddingBottom: 60 }}
-    >
+    <SafeAreaView className="flex-1 bg-white">
+      <ScrollView
+        className="flex-1 bg-white"
+        showsVerticalScrollIndicator={false}
+        contentContainerStyle={{ paddingBottom: 60 }}
+      >
 
-      {/* Header */}
-      <View className="mt-20 px-6">
-        <Text className="text-center text-[16px] text-[#4A3983] mb-10">
-          “Ensemble, on transforme les routines en moments de bonheur”
-        </Text>
+        {/* Header */}
+        <View className="mt-10 px-6">
+          <Text className="text-center text-[16px] text-[#4A3983] mb-10">
+            “Ensemble, on transforme les routines en moments de bonheur”
+          </Text>
 
-        <Text className="text-[28px] font-bold text-gray-900 mb-2">
-          Bonjour, {user?.name}
-        </Text>
+          <Text className="text-[28px] font-bold text-gray-900 mb-2">
+            Bonjour, {user?.name}
+          </Text>
 
-        <Text className="text-[16px] text-gray-700 mb-10">
-          Ton score bien-être
-        </Text>
-      </View>
+          <Text className="text-[16px] text-gray-700 mb-10">
+            Ton score bien-être
+          </Text>
+        </View>
 
-      {/* Donut */}
-      <DonutProgress score={78} progress={0.35} />
+        {/* Donut */}
+        <DonutProgress score={78} progress={0.35} />
 
-      {/* Mes défis */}
-      <View className="px-6 mb-4">
-        <Text className="text-[24px] font-bold text-gray-900">
-          Mes défis
-        </Text>
-      </View>
+        {/* Mes défis */}
+        <View className="px-6 mb-4">
+          <Text className="text-[24px] font-bold text-gray-900">
+            Mes défis
+          </Text>
+        </View>
 
-      <View className="px-6">
+        <View className="px-6">
 
-        <ChallengeCard
-          title="Écrire un journal"
-          progress={1}
-          status="Terminé"
-          color="#F2C94C"
-          finished={true}
-        />
+          <ChallengeCard
+            title="Écrire un journal"
+            progress={1}
+            status="Terminé"
+            color="#F2C94C"
+            finished={true}
+          />
 
-        <ChallengeCard
-          title="Course à pied en famille"
-          progress={0.30}
-          status="En cours…"
-          color="#A259FF"
-        />
+          <ChallengeCard
+            title="Course à pied en famille"
+            progress={0.30}
+            status="En cours…"
+            color="#A259FF"
+          />
 
-        <ChallengeCard
-          title="Déconnexion numérique"
-          progress={0.75}
-          status="En cours…"
-          color="#A259FF"
-        />
+          <ChallengeCard
+            title="Déconnexion numérique"
+            progress={0.75}
+            status="En cours…"
+            color="#A259FF"
+          />
 
-      </View>
+        </View>
 
-    </ScrollView>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
--- screens/Profile.tsx
+++ screens/Profile.tsx
@@ -1,46 +1,204 @@
-import { View, Text, Image, TouchableOpacity } from "react-native";
+import { View, Text, Image, TouchableOpacity, ScrollView } from "react-native";
 import { useAuthStore } from '../store/useAuthStore';
+import { SafeAreaView } from 'react-native-safe-area-context';
+
 
 export default function Profile() {
   const user = useAuthStore(state => state.user);
   const logout = useAuthStore(state => state.logout);
 
   return (
-    <View className="flex-1 items-center bg-white pt-20 px-6">
-      <Image
-        source={{ uri: "https://avatar.iran.liara.run/public/boy?seed=12" }}
-        className="w-32 h-32 rounded-full mb-5"
-      />
+    <SafeAreaView className="flex-1 bg-white">
+    
+      <ScrollView
+              className="flex-1 bg-white"
+              showsVerticalScrollIndicator={false}
+              contentContainerStyle={{ paddingBottom: 60 }}
+            >
 
-      <Text className="text-2xl font-bold text-gray-900 mb-2">
-        {user?.name || "Invité"}
-      </Text>
 
-      <Text className="text-gray-600 text-lg mb-4">
-        {user?.email || ""}
-      </Text>
+      {/* ———————————————————————————————-- */}
+      {/*               Avatar         */}
+      {/* ———————————————————————————————-- */}
 
-      <View className="px-6 py-2 rounded-full bg-blue-100 mt-2 mb-8">
-        <Text className="text-blue-700 font-semibold text-center">
-          {user?.isPremium ? "Premium" : "Compte Gratuit"}
-        </Text>
+        <View className="flex-2 items-center bg-white pt-20 px-6">
+          <Image
+            source={{ uri: "https://avatar.iran.liara.run/public/boy?seed=12" }}
+            className="w-32 h-32 rounded-full mb-5"
+          />
+
+          <Text className="text-2xl font-bold text-gray-900 mb-2">
+            {user?.name || "Invité"}
+          </Text>
+
+          <Text className="text-gray-600 text-lg mb-4">
+            {user?.email || ""}
+          </Text>
+
+          <View className="px-6 py-2 rounded-full bg-blue-100 mt-2 mb-8">
+            <Text className="text-blue-700 font-semibold text-center">
+              {user?.isPremium ? "Premium" : "Compte Gratuit"}
+            </Text>
+          </View>
+          
+
+      {/* ——————————————————————————————— */}
+      {/*           CARTE ACTIVITE         */}
+      {/* ——————————————————————————————— */}
+
+<View className="w-full flex-row gap-4">
+
+  {/* COLONNE GAUCHE */}
+  <View className="flex-1">
+
+    {/* Haut gauche — plus court */}
+    <View
+      className="bg-white p-4 rounded-3xl border border-gray-200 mb-4"
+      style={{ height: 150 }}
+    >
+      <Text className="text-gray-600 text-lg mb-2">Cuisine</Text>
+      <Text className="text-gray-900 text-3xl font-bold mb-1">8 pts</Text>
+      <Text className="text-gray-600 text-sm">+ 12% from last month</Text>
+    </View>
+
+    {/* Bas gauche — plus long */}
+    <View
+      className="bg-white p-4 rounded-3xl border border-gray-200"
+      style={{ height: 220 }}
+    >
+      <Text className="text-gray-600 text-lg mb-2">Développement personnel</Text>
+      <Text className="text-gray-900 text-3xl font-bold mb-1">0 pts</Text>
+      <Text className="text-gray-600 text-sm">- 14% from last month</Text>
+    </View>
+
+  </View>
+
+
+
+  {/* COLONNE DROITE */}
+  <View className="flex-1">
+
+    {/* Haut droit — plus long */}
+    <View
+      className="p-4 rounded-3xl mb-4"
+      style={{ backgroundColor: "#F2C94C", height: 220 }}
+    >
+      <Text className="text-white text-lg mb-2">Sport</Text>
+      <Text className="text-white text-3xl font-bold mb-1">12 pts</Text>
+
+      <View className="bg-white/40 px-3 py-1 rounded-full self-start mb-3">
+        <Text className="text-white">15% ↓</Text>
       </View>
 
-      {!user?.isPremium && (
-        <TouchableOpacity className="w-full bg-yellow-400 mb-6 py-3 rounded-xl mt-3" activeOpacity={0.8}>
-          <Text className="text-center font-semibold text-white">Devenir Premium</Text>
-        </TouchableOpacity>
-      )}
-
-      <TouchableOpacity
-        onPress={() => logout()}
-        className="w-full bg-red-500 py-3 rounded-xl"
-        activeOpacity={0.8}
-      >
-        <Text className="text-white font-semibold text-center text-lg">
-          Se déconnecter
-        </Text>
-      </TouchableOpacity>
     </View>
+
+    {/* Bas droit — plus court */}
+    <View
+      className="p-4 rounded-3xl"
+      style={{ backgroundColor: "#6B21A8", height: 150 }}
+    >
+      <Text className="text-white text-lg mb-2">Lecture</Text>
+      <Text className="text-white text-3xl font-bold mb-1">5 pts</Text>
+
+      <View className="bg-white/40 px-3 py-1 rounded-full self-start mb-3">
+        <Text className="text-white">12% ↑</Text>
+      </View>
+
+    </View>
+
+  </View>
+
+</View>
+
+      {/* ——————————————————————————————— */}
+      {/*         Mes thématiques         */}
+      {/* ——————————————————————————————— */}
+
+<View className="w-full px-4 mt-4 mb-8">
+
+  {/* Titre */}
+  <Text className="text-3xl font-black mb-8 mt-4">
+    Mes thématiques
+  </Text>
+
+  {/* Ligne 1 */}
+  <View className="flex-row gap-4 mb-4">
+    <View className="flex-1 border-2 border-purple-600 rounded-3xl p-6 justify-center items-center">
+      <Text className="text-purple-600 text-lg font-medium">
+        Cuisine & Nutrition
+      </Text>
+    </View>
+
+    <View className="flex-1 border-2 border-purple-600 rounded-3xl p-6 justify-center items-center">
+      <Text className="text-purple-600 text-lg font-medium">
+        Sport & Activités
+      </Text>
+    </View>
+  </View>
+
+  {/* Ligne 2 */}
+  <View className="flex-row gap-4 mb-4">
+    <View className="flex-1 border-2 border-purple-600 rounded-3xl p-6 justify-center items-center">
+      <Text className="text-purple-600 text-lg font-medium">
+        Lecture & Culture
+      </Text>
+    </View>
+
+    <View className="flex-1 border-2 border-purple-600 rounded-3xl p-6 justify-center items-center">
+      <Text className="text-purple-600 text-lg font-medium">
+        Vie sociale
+      </Text>
+    </View>
+  </View>
+
+  {/* Ligne 3 (pleine largeur) */}
+  <View className="border-2 border-purple-600 rounded-3xl p-6 justify-center items-center">
+    <Text className="text-purple-600 text-lg font-medium">
+      Développement personnel
+    </Text>
+  </View>
+
+</View>
+
+
+
+      {/* ———————————————————————————————-- */}
+      {/*           BOUTONS PREMIUM         */}
+      {/* ———————————————————————————————-- */}
+
+         {/* Devenir Premium */}
+          {!user?.isPremium && (
+              <TouchableOpacity
+                className="w-full mb-6 py-3 rounded-xl mt-8"
+                style={{ backgroundColor: "#6B21A8" }}
+                activeOpacity={0.8}
+              >
+
+                <Text className="text-center font-semibold text-white">
+                  Devenir Premium
+                </Text>
+            </TouchableOpacity>
+          )}
+
+          {/* Se déconnecter */}
+          <TouchableOpacity
+              onPress={() => logout()}
+              className="w-full py-3 rounded-xl bg-white"
+              style={{ borderWidth: 2, borderColor: "#6B21A8" }}
+              activeOpacity={0.8}
+              >
+              <Text
+                className="font-semibold text-center text-lg"
+                style={{ color: "#6B21A8" }}
+              >
+                Se déconnecter
+              </Text>
+          </TouchableOpacity>
+
+        </View>
+      </ScrollView>
+      
+    </SafeAreaView>
+    
   )
 }
